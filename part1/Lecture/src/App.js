e0adbbe7c19638144d9a94bbf0b4086d12d86b24
wonjongbot <wonjongbot@gmail.com>
2021-07-30 15:10:49 +0900

    before handling arrays

--- part1/Lecture/src/App.js
+++ part1/Lecture/src/App.js
@@ -1,5 +1,32 @@
 import React, { useState } from "react";
 
+const App = () => {
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0,
+  });
+
+  const handleLeftClick = () => 
+    setClicks({...clicks, left: clicks.left + 1,});
+  const handleRightClick = () => 
+    setClicks({...clicks, right: clicks.right + 1,});
+
+  return (
+    <div>
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
+    </div>
+  );
+};
+
+export default App;
+
+/*Part C
+
+import React, { useState } from "react";
+
 const Display = ({ counter }) => <div>{counter}</div>
 
 const Button = ({ onClick, text }) => (
@@ -26,7 +53,9 @@ const App = () => {
 };
 
 export default App;
-/* 
+
+Part B
+
 import React from "react";
 
 const Hello = ({name, age}) => {
