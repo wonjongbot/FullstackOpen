14a9f649f2b53b781887f1c9eb9fc015dbd56525
wonjongbot <wonjongbot@gmail.com>
2021-07-30 16:43:48 +0900

    part 1 D left right ting

--- part1/Lecture/src/App.js
+++ part1/Lecture/src/App.js
@@ -1,29 +1,52 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0,
-    right: 0,
-  });
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const handleLeftClick = () => 
-    setClicks({...clicks, left: clicks.left + 1,});
-  const handleRightClick = () => 
-    setClicks({...clicks, right: clicks.right + 1,});
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left+1)
+  } 
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right+1)
+  }
 
+  const Button = (props) => {
+    return(
+      <button onClick={props.onClick}>{props.text}</button>
+    )
+  }
+  const History = (props) => {
+    if (props.allClicks.length === 0) {
+      return (
+        <div>
+          the app is used by presing the buttons
+        </div>
+      )
+    }
+    return (
+      <div>
+        button press history: {props.allClicks.join(' ')}
+      </div>
+    )
+  }
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      {left}
+      <Button onClick={handleLeftClick} text={"left"}/>
+      <Button onClick={handleRightClick} text={"right"}/>
+      {right}
+      <History allClicks={allClicks}/>
     </div>
   );
 };
 
 export default App;
 
-/*Part C
+/* PART C
 
 import React, { useState } from "react";
 
