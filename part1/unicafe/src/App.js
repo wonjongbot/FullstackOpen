8517358fbdae89d77e5a65750896f8d0349151c5
wonjongbot <wonjongbot@gmail.com>
2021-08-02 12:14:38 +0900

    unicafe step 2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const Display = (props) => <div> {props.text} {props.value}</div>;
+const Display = (props) => (
+  <div>
+    {" "}
+    {props.text} {props.value}
+  </div>
+);
 
 const Button = (props) => (
   <button onClick={props.handleClick}>{props.text}</button>
@@ -8,9 +13,9 @@ const Button = (props) => (
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const setGoodValue = (newGoodValue) => {
     setGood(newGoodValue);
@@ -21,6 +26,11 @@ const App = () => {
   const setBadValue = (newBadValue) => {
     setBad(newBadValue);
   };
+  const total = good + neutral + bad;
+  const score = good - bad;
+  const average = score / total;
+  const percentage = (good / total) * 100;
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -28,11 +38,14 @@ const App = () => {
       <Button handleClick={() => setNeutValue(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBadValue(bad + 1)} text="bad" />
       <h1>statistics</h1>
-      <Display value={good} text={"good"}/>
-      <Display value={neutral} text={"neutral"}/>
-      <Display value={bad} text={"bad"}/>
+      <Display value={good} text={"good"} />
+      <Display value={neutral} text={"neutral"} />
+      <Display value={bad} text={"bad"} />
+      <Display value={total} text={"all"} />
+      <Display value={average} text={"average"} />
+      <Display value={percentage} text={"percentage"} />
     </div>
   );
-}
+};
 
-export default App
+export default App;
